f465dfc9328031b110807fd7773db88335822273
CristiVolo <cristianvolosciuc@yahoo.com>
2020-08-31 17:54:17 +0300

    7-61-Beautiful-Soup Added Bing request; printed "prettified" data (string) from the given page.

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from bs4 import BeautifulSoup
+import requests
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+search = input("Enter search term:")
+params = {"q": search}
+r = requests.get("https://www.bing.com/search?q=pizza", params=params)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+soup = BeautifulSoup(r.text, features="html.parser")
+print(soup.prettify())
